b376548c1dbce1c8b5b8f24c4fd9513103272c73
pratikshad16 <dhagepratiksha16@gmail.com>
2020-08-23 17:40:05 +0530

    Added: Functionality for resetting the board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -x
 echo "Welcome to TicTacToe Program"
+
+declare -A board
+#variables
+row=3
+column=3
+
+function resettingBoard()
+{
+	for (( i=1; i<=$row; i++ ))
+	do
+		for (( j=1; j<=$column; j++ ))
+		do
+			board[$i,$j]="-"
+		done
+	done
+}
+
+resettingBoard
